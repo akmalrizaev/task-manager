9d8ebbffcb330fcf434b3fd19d47891baa88bcd3
Akmal Rizayev <akmal.rizayev@gmail.com>
2022-10-28 17:44:32 +0500

    add firs route

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+// Route
+app.get('/hello', (req,res)=> {
+    res.send('Task Manager APP')
+})
+
 const port = 3000;
 
 app.listen(port, console.log(`Server is listening on port ${port}...`))
