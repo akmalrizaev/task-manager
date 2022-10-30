144921c47e58830c1cbeaa1d42b1621e9e95ae79
Akmal Rizayev <akmal.rizayev@gmail.com>
2022-10-30 14:20:35 +0500

    add getAllTasks

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks')
+
+
+// Middlware
+app.use(express.json())
 
 // Route
 app.get('/hello', (req,res)=> {
     res.send('Task Manager APP')
 })
+
+app.use('/api/v1/tasks',tasks)
+
+// app.get('/api/v1/tasks')....... - get all the tasks
+// app.post('/api/v1/tasks')...... - create a new task
+// app.get('/api/v1/tasks/:id')... - get single task
+// app.patch('/api/v1/tasks/:id')..- update task
+// app.delete('/api/v1/tasks/:id').- delete task
+
 
 const port = 3000;
 
