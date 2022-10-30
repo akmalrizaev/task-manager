2cd4a5e9d08a0ec91dd4f08473560c7efe975b21
Akmal Rizayev <akmal.rizayev@gmail.com>
2022-10-30 18:02:26 +0500

    Connect to DB

--- db/connect.js
+++ db/connect.js
@@ -1,2 +1,15 @@
-const connectionString = 'mongodb+srv://<username>:<password>@nodeexpressprojects.invcs8r.mongodb.net/?retryWrites=true&w=majority'
+const mongoose = require('mongoose')
+
+
+const connectionString = 'mongodb+srv://akmal:1234@nodeexpressprojects.invcs8r.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+.then(() => console.log('Connected to the DB...') )
+.catch((err) => console.log(err))
+
 
