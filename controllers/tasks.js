a7bc4e21fa13970faf1846cb76ee8926193979c5
Akmal Rizayev <akmal.rizayev@gmail.com>
2022-11-02 18:14:18 +0500

    mongoose validation try_catch

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,9 +4,16 @@ const getAllTasks = (req,res) => {
 }
 
 const createTask = async (req,res) => {
-    const task = await Task.create(req.body) 
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({task})
+    } catch (error) {
+        res.status(500).json({ msg:error })
+        
+    }
+     
     //res.send('create task')
-    res.status(201).json({task})
+    
 }
 
 const getTask = (req,res) => {
