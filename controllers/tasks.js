060cb0b08f17aefbfc24890b6c800737e970df98
Akmal Rizayev <akmal.rizayev@gmail.com>
2022-11-03 17:19:35 +0500

    Update Task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -38,8 +38,23 @@ const getTask = async (req,res) => {
     //res.send('get single task')
 }
 
-const updateTask = (req,res) => {
-    res.send('update task')
+const updateTask = async (req,res) => {
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {
+            new: true,
+            runValidators: true
+        })
+        if (!task) {
+            return res.status(404).json(`msg: No task with ID ${taskID}`)
+        }
+        res.status(200).json({task})
+        
+    } catch (error) {
+        res.status(500).json({ msg:error })
+        
+    }
+    //res.send('update task')
 }
 
 const deleteTask = async (req,res) => {
