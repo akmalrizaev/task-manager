d470aa4881cd0c6d5631358cbaf299f1c7a62b63
Akmal Rizayev <akmal.rizayev@gmail.com>
2022-11-03 16:51:40 +0500

    Delete Task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -25,6 +25,7 @@ const createTask = async (req,res) => {
 
 const getTask = async (req,res) => {
     try {
+        //console.log(req.params)
         const {id:taskID} = req.params
         const task = await Task.findOne({_id:taskID})
         if (!task) {
@@ -41,8 +42,20 @@ const updateTask = (req,res) => {
     res.send('update task')
 }
 
-const deleteTask = (req,res) => {
-    res.send('delete task')
+const deleteTask = async (req,res) => {
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOneAndDelete({_id:taskID})
+        if (!task) {
+            return res.status(404).json(`msg: No task with ID ${taskID}`)
+        }
+        res.status(200).json({task})
+        
+    } catch (error) {
+        res.status(500).json({ msg:error })
+        
+    }
+    //res.send('delete task')
 }
 
 
