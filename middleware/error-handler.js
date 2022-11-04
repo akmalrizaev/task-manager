fece87ee6fad39bf5861e8b989396c36f77c34d2
Akmal Rizayev <akmal.rizayev@gmail.com>
2022-11-04 14:45:34 +0500

    Custom Error Handler

---
+++ middleware/error-handler.js
@@ -0,0 +1,5 @@
+const errorHandlerMiddleware = (error,req,res,next) => {
+    return res.status(500).json({msg:`Something went wrong, try again later `})
+}
+
+module.exports = errorHandlerMiddleware
