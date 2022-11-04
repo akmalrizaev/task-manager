a3857603e6adcf245a3227589f2401cf24ba7a97
Akmal Rizayev <akmal.rizayev@gmail.com>
2022-11-04 15:26:55 +0500

    Custom Error Class

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require('../models/Task')
 const asyncWrapper = require('../middleware/async')
+const {createCustomError} = require('../errors/custom-error')
 const getAllTasks = asyncWrapper(async (req,res) => {
         const tasks = await Task.find({})
         res.status(200).json({tasks})
@@ -18,13 +19,16 @@ const createTask = asyncWrapper(async (req,res) => {
     
 })
 
-const getTask = asyncWrapper(async (req,res) => {
+const getTask = asyncWrapper(async (req,res,next) => {
     
         //console.log(req.params)
         const {id:taskID} = req.params
         const task = await Task.findOne({_id:taskID})
         if (!task) {
-            return res.status(404).json(`msg: No task with ID ${taskID}`)
+            // const error = new Error('Not Found')
+            // error.status = 404
+            return next(createCustomError(`msg: No task with ID ${taskID}`,404))
+            // return res.status(404).json(`msg: No task with ID ${taskID}`)
         }
         res.status(200).json(task)
     
@@ -39,7 +43,8 @@ const updateTask = asyncWrapper(async (req,res) => {
             runValidators: true
         })
         if (!task) {
-            return res.status(404).json(`msg: No task with ID ${taskID}`)
+            // return res.status(404).json(`msg: No task with ID ${taskID}`)
+            return next(createCustomError(`msg: No task with ID ${taskID}`,404))
         }
         res.status(200).json({task})
         
@@ -52,7 +57,8 @@ const deleteTask = asyncWrapper(async (req,res) => {
         const {id:taskID} = req.params
         const task = await Task.findOneAndDelete({_id:taskID})
         if (!task) {
-            return res.status(404).json(`msg: No task with ID ${taskID}`)
+            // return res.status(404).json(`msg: No task with ID ${taskID}`)
+            return next(createCustomError(`msg: No task with ID ${taskID}`,404))
         }
         res.status(200).json({task})
         
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,10 @@
-const errorHandlerMiddleware = (error,req,res,next) => {
-    return res.status(500).json({msg:`Something went wrong, try again later `})
+const {CustomApiError} = require('../errors/custom-error')
+const errorHandlerMiddleware = (err,req,res,next) => {
+    if (err instanceof CustomApiError ) {
+        return res.status(err.status).json({msg:err.message})
+    }
+    // return res.status(err.status).json({msg:err.message})
+    return res.status(500).json({msg:`Something went wrong, please try again`})
 }
 
 module.exports = errorHandlerMiddleware
